73dd173537269a3f88108522fc18520c8da69340
Sylvain Caparros <89011998+Museum79@users.noreply.github.com>
2024-03-13 13:57:06 +0100

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,26 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-import { skills } from '../data'
+import { skills } from '../data';
 
 const Skills = () => {
   return (
     <section className='bg-tertiary py-12'>
         <div className='container mx-auto'>
-            <div className='grid grid-cols-8
-                md:grid-flow-row'>
-                    {skills.map((skill, index) => {
-                        return ( 
-                        <div className='flex items-center
-                            justify-center' key={index}>
-                            <img src={skill.image} alt=''>
-                            </img>
-                        </div>
-                        )
-                    })}
+            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4'>
+                {skills.map((skill, index) => {
+                    return ( 
+                    <div className='flex flex-col items-center w-full justify-center' key={index}>
+                        <p className="text-center text-white text-md mb-2">{skill.title}</p>
+                        <img src={skill.image} alt={skill.title} className="w-16 sm:w-24">
+                        </img>
+                        
+                    </div>
+                    )
+                })}
             </div>
         </div>
     </section>
   )
 }
 
-export default Skills
+export default Skills;
