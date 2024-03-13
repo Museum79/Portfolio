66826a131c9f4607711278124d2b1be52ec486a2
Sylvain Caparros <89011998+Museum79@users.noreply.github.com>
2024-03-13 14:43:35 +0100

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,12 @@
 import React from "react";
-
 import photo from "../assets/img/photo.png";
 import { Link } from "react-scroll";
-import TypewriterComponent from "typewriter-effect";
 
 const Hero = () => {
   return (
     <section
       id="home"
-      className="lg:h-[85vh] flex
-    items-center bg-primary lg:bg-cover lg:bg-center
-    lg:bg-no-repeat py-32 lg:py-0 overflow-hidden"
+      className="lg:h-[85vh] flex items-center bg-primary lg:bg-cover lg:bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden"
     >
       <div className="container mx-auto h-full">
         <div className="flex items-center h-full pt-8">
@@ -18,20 +14,10 @@ const Hero = () => {
             <p className="text-lg text-accent mb-[22px] text-center lg:text-left w-full">
               Salut, je suis Sylvain
             </p>
-            <TypewriterComponent
-              options={{
-                strings: [
-                  '<h1 class="text-4xl leading-[44px] text-center md:text-5xl md:leading-tight lg:text-7xl lg:leading-[1.2] font-bold md:tracking-[-2px] lg:text-left w-full">Concepteur Développeur <br /> d\'applications.</h1>',
-                ],
-                autoStart: true,
-                loop: true,
-                html: true,
-              }}
-            />
-            <p
-              className="pt-4 pb-8 md:pt-6 md:pb-12
-    max-w-[380px] text-lg text-center lg:text-left w-full"
-            >
+            <h1 className="text-4xl leading-[44px] text-center md:text-5xl md:leading-tight lg:text-7xl lg:leading-[1.2] font-bold md:tracking-[-2px] lg:text-left w-full">
+              Concepteur Développeur <br /> d'applications.
+            </h1>
+            <p className="pt-4 pb-8 md:pt-6 md:pb-12 max-w-[380px] text-lg text-center lg:text-left w-full">
               Développeur en reconversion professionnelle motivé et passionné
               par l'apprentissage de nouvelles technologies.
             </p>
@@ -41,18 +27,12 @@ const Hero = () => {
               duration={700}
               offset={-70}
               style={{ cursor: "pointer" }}
-              className="btn btn-md bg-accent
-    hover:bg-accent-hover md:btn-lg
-    transition-all rounded-md"
+              className="btn btn-md bg-accent hover:bg-accent-hover md:btn-lg transition-all rounded-md"
             >
               Contactez-moi
             </Link>
           </div>
-
-          <div
-            className="hidden lg:flex flex-1
-    justify-end items-end -h-full"
-          >
+          <div className="hidden lg:flex flex-1 justify-end items-end -h-full">
             <img src={photo} alt="" className="5" />
           </div>
         </div>
